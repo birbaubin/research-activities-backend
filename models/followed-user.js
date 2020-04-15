b639a5c7232333ca8b9777bdb42bd3f62a1a56cc
aubin <aubin.birba@gmail.com>
2020-04-15 07:45:44 +0100

    added publication model

--- models/followed-user.js
+++ models/followed-user.js
@@ -14,6 +14,7 @@ const FollowedUserSchema = new Schema({
     type : String,
     required : true
   }
+
 });
 
 
---
+++ models/publication.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PublicationSchema = new Schema({
+  
+    _filled: {
+        type: Boolean,
+        required: true
+    },
+    bib: {
+        title: String,
+        year: Number
+    },
+    cited_by: {
+        type: Number,
+        required: true
+    },
+    id_citations: {
+        type: String,
+        required: true
+    },
+    source: {
+        type: String,
+        required: true
+    }
+});
+
+
+
+const Publication = mongoose.model('publication', PublicationSchema);
+module.exports = Publication;
