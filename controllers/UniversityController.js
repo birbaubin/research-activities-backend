578f4bba640f83ca7e3556e4f6854b947631a349
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-05-02 11:48:29 +0000

    Update UniversityController.js
    Prettier formatting

--- controllers/UniversityController.js
+++ controllers/UniversityController.js
@@ -1,82 +1,70 @@
-const University = require('../models/university');
-const School = require('../models/school');
+const University = require("../models/university");
+const School = require("../models/school");
 
-    
-exports.createUniversity = function(req, resp){
+exports.createUniversity = function (req, resp) {
+  console.log(req.body);
 
-        University.create(req.body)
-        .then(university =>{
-            resp.send(university);
-        })
-        .catch(error=>{
-            console.log(error);
-            resp.send("error");
-        });
-    }
+  University.create(req.body)
+    .then((university) => {
+      resp.send(university);
+    })
+    .catch((error) => {
+      console.log(error);
+      resp.send("error");
+    });
+};
 
-        
-exports.updateUniversity = function(req, resp){
+exports.updateUniversity = function (req, resp) {
+  University.updateOne({ _id: req.body._id }, { $set: req.body })
+    .then((result) => {
+      resp.send(result);
+    })
+    .catch((error) => {
+      console.log(error);
+      resp.send(error);
+    });
+};
 
-    University.updateOne({_id: req.body._id}, {$set: req.body})
-            .then(result=>{
-                resp.send(result);
-            })
-            .catch(error=>{
-                console.log(error);
-                resp.send(error)
-            })
-    
-}
+exports.findUniversity = function (req, resp) {
+  University.findById(req.params._id)
+    .then((university) => {
+      resp.send(university);
+    })
+    .catch((error) => {
+      console.log(error);
+      resp.send("error");
+    });
+};
 
-exports.findUniversity = function(req, resp){
+exports.findAllUniversities = function (req, resp) {
+  University.find()
+    .then((universities) => {
+      resp.send(universities);
+    })
+    .catch((error) => {
+      console.log(error);
+      resp.send("error");
+    });
+};
 
-    University.findById(req.params._id)
-                .then(university=>{
-                    resp.send(university);
-                })
-                .catch(error=>{
-                    console.log(error);
-                    resp.send("error");
-                })
-    
-}
+exports.deleteUniversity = function (req, resp) {
+  University.deleteOne({ _id: req.params._id })
+    .then((result) => {
+      resp.send(result);
+    })
+    .catch((error) => {
+      console.log(error);
+      resp.send(error);
+    });
+};
 
-exports.findAllUniversities = function(req, resp){
-
-   University.find().then(universities=>{
-       resp.send(universities);
-   })
-   .catch(error=>{
-       console.log(error);
-       resp.send("error");
-   })
-    
-}
-
-exports.deleteUniversity = function(req, resp){
-
-    University.deleteOne({_id: req.params._id})
-                .then(result=>{
-                    resp.send(result);
-                })
-                .catch(error=>{
-                    console.log(error);
-                    resp.send(error);
-                })
-     
- }
-
- exports.getUniversitySchools = function(req, resp){ 
-
-
-     School.find({university_id: req.params._id})
-            .then(universities=>{
-                resp.send(universities);
-            })
-            .catch(error=>{
-                console.log(error);
-                resp.send(error);
-            })
- }
-
-
+exports.getUniversitySchools = function (req, resp) {
+  School.find({ university_id: req.params._id })
+    .then((universities) => {
+      resp.send(universities);
+    })
+    .catch((error) => {
+      console.log(error);
+      resp.send(error);
+    });
+};
