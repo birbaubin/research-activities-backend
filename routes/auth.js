93c6552b9d70a2e9157ffcaa9117d062f158bd34
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-05-03 12:56:36 +0000

    Update auth.js
    Returning user short bio on login

--- routes/auth.js
+++ routes/auth.js
@@ -1,46 +1,63 @@
-const express = require('express');
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
-const config = require('../config');
+const express = require("express");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
 
+const User = require("../models/user");
+const Team = require("../models/team");
+const TeamMemberShip = require("../models/team-membership");
+const Laboratory = require("../models/laboratory");
 
 const router = express.Router();
 
-
-router.post('/signup', passport.authenticate('signup', { session : false }) , async (req, res, next) => {
+router.post(
+  "/signup",
+  passport.authenticate("signup", { session: false }),
+  async (req, res, next) => {
     res.json({
-      message : 'Signup successful',
-      user : req.user
+      message: "Signup successful",
+      user: req.user,
     });
-  });
-  
+  }
+);
 
-  router.post('/login', async (req, res, next) => {
-
-    console.log(req.body);
-    passport.authenticate('login', async (err, user, info) => {     
-        try {
-        if(err || !user){
-
-          return next(info);
-
-        }
-        req.login(user, { session : false }, async (error) => {
-          if( error ) 
-            return next(error)
-          const body = { _id : user._id, email : user.email, role: user.role };
-          const token = jwt.sign({ user : body },config.JWT_SECRET);
-          let userInfos = {};
-          Object.assign(userInfos, user._doc);
-          delete userInfos.password;
-          return res.json({ token: token, user: userInfos });
-        });     
-    } catch (error) {
-        return next(error);
+router.post("/login", async (req, res, next) => {
+  passport.authenticate("login", async (err, user, info) => {
+    try {
+      if (err || !user) {
+        return next(info);
       }
-    })(req, res, next);
-  });
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
+        const body = { _id: user._id, email: user.email, role: user.role };
+        const token = jwt.sign({ user: body }, config.JWT_SECRET);
 
+        const laboratoriesHeaded = await Laboratory.find({
+          head_id: user._id,
+        });
 
-  
-  module.exports = router;
+        const teamsMemberships = await TeamMemberShip.find({
+          user_id: user._id,
+          active: true,
+        }).then((teamsMemberships) =>
+          Promise.all(
+            teamsMemberships.map((teamsMembership) =>
+              Team.findOne({ _id: teamsMembership.team_id })
+            )
+          )
+        );
+
+        return res.json({
+          ...user._doc,
+          token,
+          laboratoriesHeaded,
+          teamsMemberships,
+        });
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+});
+
+module.exports = router;
