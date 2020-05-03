626de5711a1f8eda1ffb5461459fd79c79a60bf2
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-05-03 12:58:16 +0000

    Update auth.js
    Prettier formatting and naming improvement

--- auth/auth.js
+++ auth/auth.js
@@ -1,66 +1,84 @@
-const passport = require('passport');
-const localStrategy = require('passport-local').Strategy;
-const UserModel = require('../models/user');
-const JWTstrategy = require('passport-jwt').Strategy;
-const ExtractJWT = require('passport-jwt').ExtractJwt;
-const config = require('../config');
-const roles = require('../helpers/role')
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+const User = require("../models/user");
+const Team = require("../models/user");
+const TeamMemberShip = require("../models/team-membership");
+const Laboratory = require("../models/laboratory");
+const JWTstrategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
+const config = require("../config");
+const roles = require("../helpers/role");
 
+passport.use(
+  "signup",
+  new localStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+      passReqToCallback: true,
+    },
+    async (req, email, password, done) => {
+      try {
+        const rolesArray = [
+          roles.CED_HEAD,
+          roles.LABORATORY_HEAD,
+          roles.RESEARCHER,
+        ];
+        if (!rolesArray.includes(req.body.role)) {
+          console.log("error occured");
+        }
 
-passport.use('signup', new localStrategy({
-  usernameField : 'email',
-  passwordField : 'password',
-  passReqToCallback: true
-}, async (req, email, password, done) => {
-    try {
-      const rolesArray  = [roles.CED_HEAD, roles.LABORATORY_HEAD, roles.SEARCHER]
-      if(!rolesArray.includes(req.body.role)){
-        console.log("error occured")
-        
+        const { email, password, role } = req.body;
+
+        const user = await User.create({
+          email,
+          password,
+          role,
+        });
+
+        return done(null, user);
+      } catch (error) {
+        done(error);
       }
-        
-      const { email, password, role} = req.body;
-
-      console.log(email, password, role);
-      const user = await UserModel.create({ email, password, role, has_confirmed: false});
-      return done(null, user);
-    } catch (error) {
-      done(error);
     }
-}));
+  )
+);
 
+passport.use(
+  "login",
+  new localStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) return done(null, false, { message: "User not found" });
 
-passport.use('login', new localStrategy({
-  usernameField : 'email',
-  passwordField : 'password'
-}, async (email, password, done) => {
-  try {
-    let user = await UserModel.findOne({ email });
-    if( !user ){
-     
-      return done(null, false, { message : 'User not found'});
+        const validate = await user.isValidPassword(password);
+        if (!validate) return done(null, false, { message: "Wrong Password" });
+
+        return done(null, user, { message: "Logged in Successfully" });
+      } catch (error) {
+        return done(error);
+      }
     }
-   
-    const validate = await user.isValidPassword(password);
-    if( !validate ){
-      return done(null, false, { message : 'Wrong Password'});
+  )
+);
+
+passport.use(
+  new JWTstrategy(
+    {
+      secretOrKey: config.JWT_SECRET,
+      jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme("Bearer"),
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user);
+      } catch (error) {
+        done(error);
+      }
     }
-   
-    return done(null, user, { message : 'Logged in Successfully'});
-  } catch (error) {
-    return done(error);
-  }
-}));
-
-
-passport.use(new JWTstrategy({
- 
-  secretOrKey : config.JWT_SECRET,
-  jwtFromRequest : ExtractJWT.fromAuthHeaderWithScheme('Bearer')
-}, async (token, done) => {
-  try {
-    return done(null, token.user);
-  } catch (error) {
-    done(error);
-  }
-}));
+  )
+);
