5b052acd225d3d9fb640d868bebcb3d9b9cb0e12
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-05-06 13:05:38 +0000

    Update statistics.js
    Adding users _id to query

--- helpers/statistics.js
+++ helpers/statistics.js
@@ -1,34 +1,28 @@
 const FollowedUser = require("../models/followed-user");
 
-exports.getStatistics = function(req, resp){
+exports.getStatistics = function (req, resp) {
+  let years = [];
+  for (let i = req.query.start; i <= req.query.end; i++) years.push(i);
 
+  FollowedUser.find({}, "name id publications")
+    .then((followedUsers) => {
+      followedUsers.forEach((user) => {
+        let publicationsStatistics = {};
+        years.forEach((year) => {
+          let numberOfPublication = 0;
+          user.publications.forEach((publication) => {
+            if (publication.bib.year == year) numberOfPublication++;
+          });
 
-    let years = [];
-    for(let i = req.query.start; i<=req.query.end; i++)
-        years.push(i);
-    
-
-    FollowedUser.find({}, "name publications")
-                .then(followedUsers=>{
-                    followedUsers.forEach(user=>{
-                        let publicationsStatistics = {};
-                        years.forEach(year=>{
-                            let numberOfPublication = 0;
-                            user.publications.forEach(publication=>{
-                                if(publication.bib.year==year)
-                                    numberOfPublication++;
-                            })
-
-                            publicationsStatistics[year] = numberOfPublication;
-                    
-                        })
-                        user._doc.publicationStatistics = publicationsStatistics;
-                        delete user._doc.publications;
-                    })
-                    resp.send(followedUsers);
-                })
-                .catch(error=>{
-                    resp.status(500).send(error);
-                    console.log(error);
-                })
-}
+          publicationsStatistics[year] = numberOfPublication;
+        });
+        user._doc.publicationStatistics = publicationsStatistics;
+        delete user._doc.publications;
+      });
+      resp.send(followedUsers);
+    })
+    .catch((error) => {
+      resp.status(500).send(error);
+      console.log(error);
+    });
+};
