529fb53c35dbd7689f88d516c2bdc53c26ae14ef
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-05-22 13:01:29 +0000

    Update followed-user.js
    Updating followed user schema

--- models/followed-user.js
+++ models/followed-user.js
@@ -5,32 +5,16 @@ const Publication = require("./publication");
 const User = require("./user");
 
 const FollowedUserSchema = new Schema({
-  _filled: Boolean,
-  affiliation: String,
-  citedby: Number,
-  citedby5y: Number,
-  coauthors: [
-    {
-      _filled: Boolean,
-      affiliation: String,
-      id: String,
-      name: String,
-    },
-  ],
+  scholarId: String,
+  name: String,
   email: String,
-  hindex: Number,
-  hindex5y: Number,
-  i10index: Number,
-  id: String,
-  interests: [String],
-  name: {
-    type: String,
-    required: true,
-  },
-  publications: {
-    type: [Publication.PublicationSchema],
-  },
-  url_picture: String,
+  indexes: Array,
+  university: String,
+  coauthors: Array,
+  citationsPerYear: Array,
+  interests: Array,
+  publications: [Publication.PublicationSchema],
+  profilePicture: String,
   user_id: {
     type: Schema.Types.ObjectId,
     ref: "User",
