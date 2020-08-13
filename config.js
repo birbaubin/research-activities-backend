8803efe2efe196f633e56255e38db627d77bd54f
birbaubin <=>
2020-08-13 13:53:00 +0200

    Remove plain text env variables from config file

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ config  = {
     PORT: process.env.PORT || 8888,
     JWT_SECRET: "top_secret",
     MONGODB_URI : process.env.MONGODB_URI,
-    EMAIL_ADDRESS: "portailmarocaindesrecherches@gmail.com",
-    EMAIL_PASSWORD: "lpom,8631hsy",
+    EMAIL_ADDRESS: process.env.EMAIL_ADDRESS,
+    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
     APPLICATION_URL: "https://research-activities.netlify.app"
 }
 
